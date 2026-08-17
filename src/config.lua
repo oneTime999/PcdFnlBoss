757e0b37d5d68354bec86021a3d6f3efd807b8c8
oneTime999 <onetime.zkk@gmail.com>
2026-08-16 22:48:52 -0300

    Create config.lua

---
+++ src/config.lua
@@ -0,0 +1,46 @@
+local Config = {}
+
+Config.Title = "Pcd Fnl Boss"
+Config.Author = "onetime.999"
+
+Config.BuyInterval = 1.5
+Config.BossInterval = 2.5
+
+Config.NormalBosses = {
+    "Scarlet Carrot",
+    "Red Potato",
+    "Dark Tomato",
+    "Skull Flower",
+    "Holy Grailic",
+    "Carnivorous Jester",
+    "Pumpkin Tyrant",
+    "Golem King",
+    "Conqueror Carrot",
+}
+
+Config.EventBosses = {
+    "Dr Carrot",
+    "Dr Carrot MkI",
+    "Dr Carrot MkII",
+    "Dr Carrot MkIII",
+}
+
+Config.MerchantItems = {
+    "Gilded Hatch Hammer",
+    "Gold Scroll",
+    "Totem Of Status",
+    "Raygun",
+    "Alien Tesla",
+    "Totem Of Stars",
+    "Totem Of Might",
+    "Totem Of Marrow",
+    "Rainbow Scroll",
+    "Moonlit Scroll",
+    "Chilly Scroll",
+    "Toasty Scroll",
+    "Tranquil Scroll",
+    "Shocked Scroll",
+    "Glitched Scroll",
+}
+
+return Config
