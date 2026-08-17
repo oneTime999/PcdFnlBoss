7d7ce79de7d7e49f5062c4e60220fac4a2dfb8e4
oneTime999 <onetime.zkk@gmail.com>
2026-08-17 00:17:40 -0300

    Update config.lua

--- src/config.lua
+++ src/config.lua
@@ -1,56 +1,120 @@
 local Config = {}
 
-Config.Title = "Pcd Fnl Boss"
-Config.Author = "onetime.999"
-Config.Version = "1.5.0"
+Config.Version = "2.0.0"
 
-Config.BuyInterval = 1.5
-Config.BuyRemoteDelay = 0.15
-Config.BossInterval = 2.5
-Config.BossRemoteDelay = 1.25
-Config.WorkerErrorDelay = 1.0
-
-Config.ShopNames = {
-    Capybaras = "EggShop",
-    Gears = "GearShop",
-    Merchant = "MerchantShop",
+Config.UI = {
+    Title = "Pcd Fnl Boss",
+    Author = "onetime.999",
+    Theme = "Default",
+    ToggleKey = "K",
+    RayfieldURL = "https://sirius.menu/rayfield",
 }
 
-Config.NormalBosses = {
-    "Scarlet Carrot",
-    "Red Potato",
-    "Dark Tomato",
-    "Skull Flower",
-    "Holy Grailic",
-    "Carnivorous Jester",
-    "Pumpkin Tyrant",
-    "Golem King",
-    "Conqueror Carrot",
+Config.Timing = {
+    WorkerErrorDelay = 1.0,
+
+    BuyLoop = 1.5,
+    BuyRemote = 0.15,
+    SameStockCooldown = 2.5,
+
+    BossLoop = 2.5,
+    BossRemote = 1.25,
+
+    DynamicOptionsRefresh = 3.0,
 }
 
-Config.EventBosses = {
-    "Dr Carrot",
-    "Dr Carrot MkI",
-    "Dr Carrot MkII",
-    "Dr Carrot MkIII",
+Config.Shops = {
+    Capybaras = {
+        Label = "Capybaras",
+        DropdownLabel = "Select Capybaras",
+        ToggleLabel = "Auto Buy Capybaras",
+
+        UIName = "EggShop",
+        Remote = "BuyItem",
+
+        DynamicOptions = true,
+    },
+
+    Gears = {
+        Label = "Gears",
+        DropdownLabel = "Select Gears",
+        ToggleLabel = "Auto Buy Gears",
+
+        UIName = "GearShop",
+        Remote = "BuyItem",
+
+        DynamicOptions = true,
+    },
+
+    Merchant = {
+        Label = "Merchant",
+        DropdownLabel = "Select Merchant Items",
+        ToggleLabel = "Auto Buy Merchant",
+
+        UIName = "MerchantShop",
+        Remote = "BuyMerchantItem",
+
+        DynamicOptions = false,
+
+        Options = {
+            "Gilded Hatch Hammer",
+            "Gold Scroll",
+            "Totem Of Status",
+            "Raygun",
+            "Alien Tesla",
+            "Totem Of Stars",
+            "Totem Of Might",
+            "Totem Of Marrow",
+            "Rainbow Scroll",
+            "Moonlit Scroll",
+            "Chilly Scroll",
+            "Toasty Scroll",
+            "Tranquil Scroll",
+            "Shocked Scroll",
+            "Glitched Scroll",
+        },
+    },
 }
 
-Config.MerchantItems = {
-    "Gilded Hatch Hammer",
-    "Gold Scroll",
-    "Totem Of Status",
-    "Raygun",
-    "Alien Tesla",
-    "Totem Of Stars",
-    "Totem Of Might",
-    "Totem Of Marrow",
-    "Rainbow Scroll",
-    "Moonlit Scroll",
-    "Chilly Scroll",
-    "Toasty Scroll",
-    "Tranquil Scroll",
-    "Shocked Scroll",
-    "Glitched Scroll",
+Config.ShopOrder = {
+    "Capybaras",
+    "Gears",
+    "Merchant",
+}
+
+Config.BossGroups = {
+    Normal = {
+        Section = "Bosses",
+        DropdownLabel = "Select Bosses",
+        ToggleLabel = "Auto Summon Bosses",
+        ButtonLabel = "Summon Selected Bosses",
+
+        Options = {
+            "Scarlet Carrot",
+            "Red Potato",
+            "Dark Tomato",
+            "Skull Flower",
+            "Holy Grailic",
+            "Carnivorous Jester",
+            "Pumpkin Tyrant",
+            "Golem King",
+            "Conqueror Carrot",
+        },
+    },
+
+    Event = {
+        Section = "Dr Carrot Challenge",
+        DropdownLabel = "Select Event Bosses",
+        ToggleLabel = "Auto Summon Event Bosses",
+        ButtonLabel = "Summon Selected Event Bosses",
+
+        Options = {
+            "Dr Carrot",
+            "Dr Carrot MkI",
+            "Dr Carrot MkII",
+            "Dr Carrot MkIII",
+        },
+    },
 }
 
 return Config
